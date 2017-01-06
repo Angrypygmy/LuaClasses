08fc2a58671be95e9e1933b6d3918fabdd4d0daa
Angrypygmy <gibsonlpsb@gmail.com>
2017-01-06 02:11:04 -0500

    Update and rename readMeFirst.lua to READMEFIRST.lua

--- READMEFIRST.lua
+++ READMEFIRST.lua
@@ -2,6 +2,10 @@
   -- If you read this from top to bottom it might not make sense. If you are looking at 'Method 1' in a section,
   -- follow all the other sections that have a 'Method 1' before reading a 'Method 2'. The same goes for anywhere you see 'Method 2'.
 
+  -- Whenever you create a class, it will create Setter and Getter functions automatically based off of your variable name choices.
+  -- This keeps your variables encapsulated, creating a more secure environment. The only way to access them is with those functions.
+  -- The only exception where it wont make setters and getters is if you declare functions within the table, which is absolutely acceptable.
+
 -- Usage:
 
 -- require class.lua somewhere in your script.
